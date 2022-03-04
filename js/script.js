06c753512ee2f6cfdba56427fc00b075e7e41edf
keyboardTC <37123270+keyboardTC@users.noreply.github.com>
2022-03-03 23:11:48 -0500

    header design dafted

---
+++ js/script.js
@@ -0,0 +1,36 @@
+"strict mode";
+
+console.log("My js Script");
+
+const productsAr = [
+    {
+      prod_img: 'prod1.jpg',
+      name: 'Product3',
+      brand: "colourpop",
+      description: 'Here is a shot of this product that might entice a user to click and add it to their cart.',
+      price: 50.00,
+      price_sign: "$",
+      product_colors: ['Fair 05', 'Ziggie'],
+      category: "lipstick"
+    },
+    {
+        prod_img: 'prod1.jpg',
+        name: 'Blotted Lip',
+        brand: "colourpop",
+        description: 'Here is a shot of this product that might entice a user to click and add it to their cart.',
+        price: 50.00,
+        price_sign: "$",
+        product_colors: ['Fair 05', 'Ziggie'],
+        category: "lipstick"
+    },
+    {
+        prod_img: 'prod1.jpg',
+        name: 'Lippie Pencil',
+        brand: "colourpop",
+        description: 'Here is a shot of this product that might entice a user to click and add it to their cart.',
+        price: 50.00,
+        price_sign: "$",
+        product_colors: ['Fair 05', 'Ziggie'],
+        category: "lipstick"
+    }
+  ]
